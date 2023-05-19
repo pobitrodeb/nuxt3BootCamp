faea03bff344c7f3ece84f662c3e2209c644aab9
Pobitrodeb <pobitrodn@gmail.com>
2023-05-19 20:01:52 +0600

    006 Building the Home Hero session done

--- app.vue
+++ app.vue
@@ -7,5 +7,21 @@
         <a href="" class="text-3xl fontt-mono">cartrader</a>
        </header>
        <!-- -- NAVBAR --  -->
+
+        <!-- --Start Home Hero --  -->
+        <div class="relative h-screen w-100 items-center bg-no-repat bg-cover bg-bttom bg-[url('https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_960_720.jpg')]">
+        
+          <div class="z-10 absolute w-full h-full flex justify-center items-center">
+          <div class="text-center w-1/2 ">
+              <h1 class="text-8xl font-serif mb-1 text-white"> Find your drive </h1>
+          </div>
+
+          <div class="z-1 absolute w-full h-full bg-black opacity-50">
+
+          </div>
+       </div>
+
+        </div>
+        <!-- --End Home Hero --  -->
   </div>
 </template>
