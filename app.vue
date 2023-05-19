28a6cadde212ffcb58efeadcfb786e6e78a8d793
Pobitrodeb <pobitrodn@gmail.com>
2023-05-19 19:25:11 +0600

    005 Building the NavBar session done

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+       <!-- -- NAVBAR --  -->
+       <header 
+       class="sticky top-0 z-50 flex justify-between item-center space-x-1 border-b bg-white p-4  shadow-md">
+        <a href="" class="text-3xl fontt-mono">cartrader</a>
+       </header>
+       <!-- -- NAVBAR --  -->
   </div>
 </template>
