5ca755c186852204dbfac58ed897f983e820e58b
Pobitrodeb <pobitrodn@gmail.com>
2023-05-19 21:05:45 +0600

    008 Building the Side Bar session done

--- app.vue
+++ app.vue
@@ -1,6 +1,47 @@
 <template>
   <div>
-    <!-- <NuxtWelcome /> -->
+    <header
+      class="sticky top-0 z-50 flex justify-between item-center space-x-1 border-b bg-white p-4 shadow-md"
+    >
+      <a href="" class="text-3xl fontt-mono">cartrader</a>
+    </header>
+    <!-- -- Car Page --  -->
+    <div
+      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+    >
+      <div class="mt-32 flex">
+        <!-- -- Car Sidebar --  -->
+        <div class="shadow border w-64 mr-10 z-30 h-[190px]">
+          <div
+            class="py-5 flex justify-between relative cursor-pointer border-b"
+            >
+            <h3>Location</h3>
+            <h3 class="text-blue-400 capitalize p-2">Toron</h3>
 
+            <!-- <div class="absolute border shadow left-64 p-5 top-1 -m-1 bg-white">
+              <input type="text" class="border p-1 rounded" />
+              <button
+                class="bg-blue-400 w-full mt-2 rounded text-white p-1"
+              > Apply </button>
+            </div> -->
+          </div>
+
+          <div class="py-5 flex justify-between relative cursor-pointer border-b">
+            <h3>Make</h3>
+            <h3 class="text-blue-400 capitalize p-2">Toyota</h3>
+          </div>
+
+          <div class="py-5 flex justify-between relative cursor-pointer border-b">
+            <h3>Price</h3>
+            <h3 class="text-blue-400 capitalize p-2">Any</h3>
+          </div>
+        </div>
+        <!-- -- Car Sidebar --  -->
+
+        <!-- -- Car Cards  --  -->
+        <!-- -- Car Cards  --  -->
+      </div>
+    </div>
+    <!-- -- Car Page --  -->
   </div>
 </template>
