db866000cddadc0af52d9abfe3f836997766d0ac
Pobitrodeb <pobitrodn@gmail.com>
2023-05-20 21:51:35 +0600

    004 Filling the Page With Components session done

--- components/CarSideBar.vue
+++ components/CarSideBar.vue
@@ -1,19 +1,15 @@
-<!-- CARS PAGE -->
-<div
-  class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
->
-  <div class="mt-32 flex">
-    <!-- CAR SIDE BAR -->
-    <div class="shadow border w-64 mr-10 z-30 h-[190px]">
+<template>
+ <!-- -- Car Sidebar --  -->
+ <div class="shadow border w-64 mr-10 z-30 h-[190px]">
       <div class="p-5 flex justify-between relative cursor-pointer border-b">
         <h3>Location</h3>
         <h3 class="text-blue-400 capitalize">Toronto</h3>
         <!-- <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
-              <input type="text" class="border p-1 rounded" />
-              <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">
-                Apply
-              </button>
-            </div> -->
+            <input type="text" class="border p-1 rounded" />
+            <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">
+              Apply
+            </button>
+          </div> -->
       </div>
 
       <div class="p-5 flex justify-between relative cursor-pointer border-b">
@@ -26,13 +22,5 @@
         <h3 class="text-blue-400 capitalize"></h3>
       </div>
     </div>
-    <!-- CAR SIDE BAR -->
-
-    <!-- CAR CARDS -->
-    <div class="w-full">
-     
-    </div>
-    <!-- CAR CARDS -->
-  </div>
-</div>
-<!-- CARS PAGE -->
+    <!-- -- Car Sidebar --  -->
+</template>
--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-        File path is : City/City/Car/make.vue 
-    </div>
+    <NavBar />
+    <div
+  class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+>
+  <div class="mt-32 flex">
+   
+   <CarSideBar/>
+
+    <CarCard />
+   
+  </div>
+</div>
+
 </template>
