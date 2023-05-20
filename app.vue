5529a4df5d42397776eddbb20f6275a31fd6718c
Pobitrodeb <pobitrodn@gmail.com>
2023-05-20 20:10:38 +0600

    010 Building the Car Details Page session done

--- app.vue
+++ app.vue
@@ -1,12 +1,5 @@
 <template>
   <div>
-    <header
-      class="sticky top-0 z-50 flex justify-between item-center space-x-1 border-b bg-white p-4 shadow-md"
-    >
-      <a href="" class="text-3xl fontt-mono">cartrader</a>
-    </header>
-    <!-- -- Car Page --  -->
-  
-    <!-- -- Car Page --  -->
+
   </div>
 </template>
