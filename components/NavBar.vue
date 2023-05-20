58408f82b144d342286f790f66d832f786064655
Pobitrodeb <pobitrodn@gmail.com>
2023-05-20 22:58:05 +0600

    006 The NuxtLink Component session done

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,7 +3,8 @@
   <header
     class="sticky top-0 z-50 flex justify-between item-center space-x-1 border-b bg-white p-4 shadow-md"
   >
-    <a href="" class="text-3xl fontt-mono">cartrader</a>
+    <!-- <a href="/" class="text-3xl fontt-mono">cartrader</a> -->
+    <NuxtLink class="text-3xl fontt-mono" to="/">  </NuxtLink>
   </header>
   <!-- -- NAVBAR --  -->
 </template>
