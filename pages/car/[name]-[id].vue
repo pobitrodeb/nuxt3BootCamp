60e0b64d57b62e63140650f2391e046535a7f4d1
Pobitrodeb <pobitrodn@gmail.com>
2023-05-22 23:17:54 +0600

    002 Defining a Custom Layout session done

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -4,6 +4,10 @@
     title: toTitleCase( route.params.name )
   })
 
+definePageMeta({
+  layout: "custom"
+});
+
   function toTitleCase(str) {
   return str.replace(
     /\w\S*/g,
@@ -17,14 +21,14 @@
 <template>
   <NavBar />
   <!-- CAR DETAIL PAGE -->
-  <div
-    class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
-  >
-
+  <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
+ 
+   <div>
     <CarDetailHero/>
     <CardDetailAttribute/>
     <CarDetailDescription/>
     <CarDetailContact />
-  </div>
+   </div>
+</div>
   <!-- CAR DETAIL PAGE   -->
 </template>
